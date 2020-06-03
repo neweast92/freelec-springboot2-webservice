717af917d9438b9d496134def48a28b4ebb44b0b
neweast92 <solyd123@daum.net>
2020-06-03 10:59:27 +0900

    20-06-03 commit

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,20 @@
+var main={
+    init : function(){
+        var _this = this;
+        $('#btn-save').on('click',function(){
+            _this.save();
+        });
+    },
+
+    save : function(){
+        var data = {
+            title: $('#title').val();
+            author: $('#author').val();
+            content: $('#content').val();
+        };
+
+        $.ajax({
+
+        });
+    }
+}
